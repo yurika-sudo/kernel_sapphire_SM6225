2720ae93977f754d25050b37329481240a1a4d45
yu <yurika0uu@gmail.com>
2026-05-22 04:44:00 +0700

    Remove CLO cloning option from clone-source.sh
    Removed CLO repository cloning from the script.

--- scripts/clone-source.sh
+++ scripts/clone-source.sh
@@ -17,13 +17,6 @@ case "$SOURCE_TYPE" in
     git clone --recursive --branch "$GKI_BRANCH" "$GKI_REPO" "$KERNEL_SRC" --depth=1
     ;;
 
-  clo)
-    CLO_REPO="https://git.codelinaro.org/clo/la/kernel/msm-5.15"
-    CLO_BRANCH="kernel.lnx.5.15.r18-rel"
-    echo "[CLO] Cloning $CLO_BRANCH ..."
-    git clone --recursive --branch "$CLO_BRANCH" "$CLO_REPO" "$KERNEL_SRC" --depth=1
-    ;;
-
   *)
     echo "[ERROR] Unknown source type: $SOURCE_TYPE"
     exit 1
