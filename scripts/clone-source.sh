d99a65f7be4ccc96030633b387bbb1d68339972e
yu <yurika0uu@gmail.com>
2026-05-24 07:51:56 +0700

    add CLO kernel source support to clone-source.sh

--- scripts/clone-source.sh
+++ scripts/clone-source.sh
@@ -17,6 +17,13 @@ case "$SOURCE_TYPE" in
     git clone --recursive --branch "$GKI_BRANCH" "$GKI_REPO" "$KERNEL_SRC" --depth=1
     ;;
 
+  clo)
+    CLO_REPO="https://git.codelinaro.org/clo/la/kernel/msm-5.15"
+    CLO_BRANCH="kernel.lnx.5.15.r1-rel"
+    echo "[CLO] Cloning $CLO_BRANCH ..."
+    git clone --recursive --branch "$CLO_BRANCH" "$CLO_REPO" "$KERNEL_SRC" --depth=1
+    ;;
+
   *)
     echo "[ERROR] Unknown source type: $SOURCE_TYPE"
     exit 1
