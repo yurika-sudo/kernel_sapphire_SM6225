4824c5637e4be2db03fcea9ed436c9315e6c8f7e
yu <yurika0uu@gmail.com>
2026-06-29 10:57:35 +0700

    Remove DEBUG_LIST and DEBUG_MISC from configs script
    Removed DEBUG_LIST and DEBUG_MISC from the symbol stripping list.

--- scripts/setup-configs.sh
+++ scripts/setup-configs.sh
@@ -19,7 +19,6 @@ sed -i 's/CONFIG_CMDLINE="/&slub_debug=- page_owner=off /' "$CF"
 # Strip symbols already in base defconfig to avoid "reassigning" warnings
 for SYM in PID_NS \
            DEBUG_KINFO \
-           DEBUG_LIST DEBUG_MISC \
            LRU_GEN LRU_GEN_ENABLED NET_SCH_FQ DEBUG_MEMORY_INIT PRINTK_CALLER \
            ZRAM_DEF_COMP_LZORLE ZRAM_DEF_COMP_ZSTD ZRAM_DEF_COMP_LZ4 ZRAM_DEF_COMP_LZO ZRAM_DEF_COMP; do
   sed -i "/^CONFIG_${SYM}[= ]/d; /^# CONFIG_${SYM} /d" "$CF"
