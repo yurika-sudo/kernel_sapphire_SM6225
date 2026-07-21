b5d4ba0418d68b47631d39a12e619c4f8f021fc0
yu <yurika0uu@gmail.com>
2026-07-22 03:33:58 +0700

    Update ZRAM and ZSMALLOC configurations in setup script

--- scripts/setup-configs.sh
+++ scripts/setup-configs.sh
@@ -13,10 +13,12 @@ CF="$DEFCONFIG"
 # Base cleanup
 sed -i 's/ cgroup_disable=pressure//'                    "$CF"
 sed -i 's/CONFIG_CMDLINE="/&slub_debug=- page_owner=off noirqdebug mitigations=off /' "$CF"
+sed -i 's/kasan\.stacktrace=off/kasan=off/'               "$CF"
 
 # Strip symbols already in base defconfig to avoid "reassigning" warnings
 for SYM in PID_NS DEBUG_KINFO \
            NET_SCH_CODEL NET_SCH_FQ_CODEL UBSAN \
+           ZRAM ZSMALLOC \
            LRU_GEN LRU_GEN_ENABLED NET_SCH_FQ DEBUG_MEMORY_INIT PRINTK_CALLER \
            ZRAM_DEF_COMP_LZORLE ZRAM_DEF_COMP_ZSTD ZRAM_DEF_COMP_LZ4 ZRAM_DEF_COMP_LZO ZRAM_DEF_COMP \
            SCHED_WALT SCHED_WALT_DEBUG CPU_FREQ_GOV_SCHEDUTIL CPU_FREQ_DEFAULT_GOV_SCHEDUTIL; do
@@ -42,8 +44,10 @@ CONFIG_NET_SCH_PIE=y
 CONFIG_MQ_IOSCHED_DEADLINE=y
 CONFIG_DEFAULT_MQ_IOSCHED="mq-deadline"
 CONFIG_DEFAULT_DEADLINE=y
+CONFIG_ZRAM=y
+CONFIG_ZSMALLOC=y
 CONFIG_ZRAM_WRITEBACK=y
-# CONFIG_ZRAM_MEMORY_TRACKING is not set
+CONFIG_ZRAM_MEMORY_TRACKING=y
 # CONFIG_ZRAM_DEF_COMP_LZORLE is not set
 # CONFIG_ZRAM_DEF_COMP_ZSTD is not set
 CONFIG_ZRAM_DEF_COMP_LZ4=y
